2ad91f0f3636a73c87c612f93eb158df4e55a123
julia <julia.rus@gmail.com>
2026-03-25 10:40:43 +0100

    feat: Añadido getters y setters en la clase Cita

--- Cita.java
+++ Cita.java
@@ -6,5 +6,54 @@ public class Cita {
 	private Cliente cliente;
 	private Coche coche;
 	private Factura factura;
-	private Empleado empleado; 
+	private Empleado empleado;
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public Coche getCoche() {
+		return coche;
+	}
+
+	public Factura getFactura() {
+		return factura;
+	}
+
+	public Empleado getEmpleado() {
+		return empleado;
+	}
+	
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+	
+	public void setCoche(String coche) {
+		this.coche = coche;
+	}
+	
+	public void setFactura(String factura) {
+		this.factura = factura;
+	}
+	
+	public void setEmpleado(String empleado) {
+		this.empleado = empleado;
+	}
+
 }

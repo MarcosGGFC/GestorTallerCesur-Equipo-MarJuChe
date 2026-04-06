e7a63bf65c7497ddd50a5492599dd4772a4b305f
julia <julia.rus@gmail.com>
2026-04-06 21:12:19 +0200

    Añadir método guardarEmpleados en Persistencia

--- Persistencia.java
+++ Persistencia.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,14 +10,49 @@ public class Persistencia {
 
     private static final String FICHERO_EMPLEADOS = "empleados.txt";
 
-    public static void guardarEmpleados(ArrayList<Empleado> lista) {
-        try (PrintWriter pw = new PrintWriter(new FileWriter(FICHERO_EMPLEADOS))) {
-            for (Empleado e : lista) {
-                pw.println(e.getNombre() + "," + e.getPuesto() + "," + e.getSueldo() + "," + e.getTurno() + "," + e.isEstaDisponible());
+    public static void guardarEmpleados(ArrayList<Empleado> listaEmpleados) {
+        try (PrintWriter escritor = new PrintWriter(new FileWriter(FICHERO_EMPLEADOS))) {
+            for (Empleado empleadoActual : listaEmpleados) {
+                escritor.println(empleadoActual.getNombre() + "," + empleadoActual.getPuesto() + "," + empleadoActual.getSueldo() + "," + empleadoActual.getTurno() + "," + empleadoActual.isEstaDisponible());
             }
-        } catch (IOException e) {
-            System.out.println("Error al guardar: " + e.getMessage());
+        } catch (IOException error) {
+            System.out.println("Error al guardar: " + error.getMessage());
         }
+    }
+
+    public static ArrayList<Empleado> cargarEmpleados() {
+        ArrayList<Empleado> listaEmpleados = new ArrayList<>();
+        File archivoEmpleados = new File(FICHERO_EMPLEADOS);
+        
+        if (!archivoEmpleados.exists()) {
+            return listaEmpleados;
+        }
+
+        try (BufferedReader lector = new BufferedReader(new FileReader(archivoEmpleados))) {
+            String lineaTexto;
+            while ((lineaTexto = lector.readLine()) != null) {
+                String[] datosEmpleado = lineaTexto.split(",");
+                
+                if (datosEmpleado.length == 5) {
+                    String nombre = datosEmpleado[0];
+                    String puesto = datosEmpleado[1];
+                    double sueldo = Double.parseDouble(datosEmpleado[2]);
+                    String turno = datosEmpleado[3];
+                    String disponibilidad = datosEmpleado[4];
+
+                    Empleado nuevoEmpleado = new Empleado(nombre, puesto, sueldo, turno);
+                    
+                    if (disponibilidad.equals("false")) {
+                        nuevoEmpleado.cambiarEstado();
+                    }
+                    
+                    listaEmpleados.add(nuevoEmpleado);
+                }
+            }
+        } catch (Exception error) {
+            System.out.println("Error al cargar: " + error.getMessage());
+        }
+        return listaEmpleados;
     }
 
 }

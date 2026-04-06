f0c944a0cf6ce73ea966023aaedb71274b4c704a
julia <julia.rus@gmail.com>
2026-04-06 21:05:51 +0200

    Añadir método guardarEmpleados en Persistencia

--- Persistencia.java
+++ Persistencia.java
@@ -1,11 +1,20 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Persistencia {
+
+    private static final String FICHERO_EMPLEADOS = "empleados.txt";
+
+    public static void guardarEmpleados(ArrayList<Empleado> lista) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(FICHERO_EMPLEADOS))) {
+            for (Empleado e : lista) {
+                pw.println(e.getNombre() + "," + e.getPuesto() + "," + e.getSueldo() + "," + e.getTurno() + "," + e.isEstaDisponible());
+            }
+        } catch (IOException e) {
+            System.out.println("Error al guardar: " + e.getMessage());
+        }
+    }
 
 }
